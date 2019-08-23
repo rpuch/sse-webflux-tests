6ab20f33178f2c298ffbc16e4ab354bfc73eea0a
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-08-23 22:51:08 +0400

    Remove an unneeded @Component

--- src/main/java/com/rpuch/test/ssewebflux/PersonsController.java
+++ src/main/java/com/rpuch/test/ssewebflux/PersonsController.java
@@ -2,14 +2,12 @@ package com.rpuch.test.ssewebflux;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@Component
 public class PersonsController {
     private final Service service;
 
